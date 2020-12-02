911ef58ef5c0f71b761edf50e8fac024daf4ee6b
Kristopher Merolla <27740303+kristopher-merolla@users.noreply.github.com>
2020-12-02 14:01:30 -0800

    adding some notes and challenges

--- data_structures.py
+++ data_structures.py
@@ -18,25 +18,58 @@
 # a->b->c->d
 
 # a, b, c, and d above are nodes which is a class that has two attributes:
-class SLNode:
+class SLNode: # first node in a list is the head
     def __init__(self, value):
         self.value = value # whatever the value of the node is for each node
         self.next = None # the link to the next node in the list, or set to None if no next node
 
-# First node in a list is the head
+# The list should also have some functionality
 class SList:
     def __init__(self):
-    	self.head = None
+        self.head = None
 
-    def add_to_front(self, value):
-        new_node = SLNode(value)
-        new_node.next = self.head
+    def add_to_front(self, val): # add a value to the front of the list
+        new_node = SLNode(val)
+        current_head = self.head
+        new_node.next = current_head
         self.head = new_node
+        return self
+
+    def print_values(self): # print out the values in the list
+        runner = self.head
+        while (runner != None):
+            print(runner.value)
+        runner = runner.next
+        return self
+
+    def add_to_back(self, val): # add an value to the end of a list
+        if self.head == None:	# if the list is empty, add to front!
+            self.add_to_front(val)
+            return self
+        new_node = SLNode(val)
+        runner = self.head
+        while (runner.next != None):
+            runner = runner.next
+        runner.next = new_node
+        return self
+
+    def remove_from_front(self): # remove the first node and return it's value
+        pass
+
+    def remove_from_back(self):
+        pass
+
+    def remove_value(self, value):
+        pass
+
+    def insert_at(self, value, n):
+        pass
 
 # New list example
 my_list = SList() # create the list
-# populate list, in order "a->b->c->d"
-my_list.add_to_front("d")
-my_list.add_to_front("c")
-my_list.add_to_front("b")
-my_list.add_to_front("a")
+
+my_list.add_to_front("d") # "d"
+my_list.add_to_front("c") # "c->d"
+my_list.add_to_front("b") # "b->c->d"
+my_list.add_to_front("a") # populate list, final order "a->b->c->d"
+my_list.print_values()
