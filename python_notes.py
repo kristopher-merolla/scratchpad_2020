8e0d9f7549e565b5a93ee772d83ce3d1561037c0
Kristopher Merolla <27740303+kristopher-merolla@users.noreply.github.com>
2020-12-02 13:49:33 -0800

    syntax cleanup and convention note

--- python_notes.py
+++ python_notes.py
@@ -2,6 +2,8 @@
 ## -- Notes on Python (Mac) -- ##
 #################################
 
+# Python style guide: https://www.python.org/dev/peps/pep-0008/
+
 # Creating a python virtual environment
 python3 -m venv py3Env 
 
@@ -102,18 +104,21 @@ def add(x,y):
     z = x + y
     return z
 
-def makeStuff(name='', time=0): # set default when declaring a param
+def make_stuff(name='', time=0): # set default when declaring a param
 
 new_num = add(4,5)
 print(new_num)
 
-def funInFunction(arg, *args): # the asterisk is called the splat operator
+def fun_in_function(arg, *args): # the asterisk is called the splat operator
     print(arg," and ",*args) # *args appear as a tuple
 
-def anotherFunction(*args,**kwargs): # arguments and key-word arguments
+def another_function(*args,**kwargs): # arguments and key-word arguments
     print(*args)
 
 ## Classes
+
+# Class names should normally use the CapWords convention.
+
 class User:
     def __init__(self):
         self.name = "Kristopher" # classes have attributes
@@ -276,7 +281,7 @@ my_project
 ## Python testing framework
 import unittest # import the python testing framework
 
-def isEven(n): ## Example function isEven which is tested
+def is_even(n): ## Example function isEven which is tested
     if n % 2 == 0:
        return True
     else:
@@ -284,20 +289,20 @@ def isEven(n): ## Example function isEven which is tested
 
 class IsEvenTests(unittest.TestCase): ## Unit test (test_even.py)
     # each method in this class is a test to be run
-    def testTwo(self):
+    def test_two(self):
         self.assertEqual(isEven(2), True)
         # another way to write above is
         self.assertTrue(isEven(2))
-    def testThree(self):
+    def test_three(self):
         self.assertEqual(isEven(3), False)
         # another way to write above is
         self.assertFalse(isEven(3))
-    # any task you want run before any method above is executed, put them in the setUp method
-    def setUp(self):
-        # add the setUp tasks
-        print("running setUp")
+    # any task you want run before any method above is executed, put them in the set_up method
+    def set_up(self):
+        # add the set_up tasks
+        print("running set_up")
     # any task you want run after the tests are executed, put them in the tearDown method
-    def tearDown(self):
+    def tear_down(self):
         # add the tearDown tasks
         print("running tearDown tasks")
 if __name__ == '__main__':
