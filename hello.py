d59910bf8bc99159939cf901bd22826adb14e0af
Kristopher Merolla <27740303+kristopher-merolla@users.noreply.github.com>
2020-12-03 11:56:30 -0800

    Added additional example routes to sample flask server

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+# Example "Hello World" server
 
 # For flask 1.0.2
+
+# Do not run in app, run with:
+# FLASK_APP=hello.py flask run
 
 from flask import Flask
 app = Flask(__name__)
@@ -8,5 +12,19 @@ app = Flask(__name__)
 def hello():
     return "Hello World!" 
 
-# Do not run in app, run with:
-# FLASK_APP=hello.py flask run
+# Additional example routes:
+
+@app.route('/success')
+def success():
+  return "success"
+
+@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+    print(name)
+    return "hello "+name
+
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
